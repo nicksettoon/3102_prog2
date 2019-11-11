13e6a412f170008c5d37f815f66739450bc3f27c
nicksettoon <nwsettoon+github@gmail.com>
2019-11-11 11:33:40 -0600

    removed files

--- main.cpp
+++
@@ -1,6 +0,0 @@
-#include "tools.h"
-#include "trie.h"
-
-// namespace node = tnode::Node;
-// namespace prt = tools::print;
-// namespace str = std::string;
--- tools.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <iostream>
-#include <string>
-
-
-void print(std::string s)
-{//prints single string to output
-    std::cout << s << std::endl;
-}
